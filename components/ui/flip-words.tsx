1ce0b1bcef4a43c6e56c825121d0ae3deb4e5b4d
Tharusha Perera <tharushaperera088@gmail.com>
2024-05-18 18:03:00 +0530

    completed the base structure

---
+++ components/ui/flip-words.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+import { cn } from "@/utils/cn";
+let interval: any;
+
+export const FlipWords = ({
+  words,
+  duration = 3000,
+  className,
+}: {
+  words: string[];
+  duration?: number;
+  className?: string;
+}) => {
+  const [currentWord, setCurrentWord] = useState(words[0]);
+
+  useEffect(() => {
+    startAnimation();
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const startAnimation = () => {
+    let i = 0;
+    interval = setInterval(() => {
+      i++;
+      if (i === words.length) {
+        i = 0;
+      }
+      const word = words[i];
+      setCurrentWord(word);
+    }, duration);
+  };
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
+        exit={{
+          opacity: 0,
+          y: -40,
+          x: 40,
+          filter: "blur(8px)",
+          scale: 2,
+          position: "absolute",
+        }}
+        className={cn(
+          "z-10 inline-block relative text-left text-purple text-lg md:text-xl lg:text-2xl",
+          className
+        )}
+        key={currentWord}
+      >
+        <motion.span
+          key={currentWord + currentWord.length}
+          initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          transition={{
+            delay: currentWord.length * 0.08,
+            duration: 0.4,
+          }}
+          className="inline-block"
+        >
+          {currentWord}
+        </motion.span>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
